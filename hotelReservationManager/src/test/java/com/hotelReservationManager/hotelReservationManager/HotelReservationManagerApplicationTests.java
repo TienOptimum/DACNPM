1ff5e63d41f22b9a3ff9f97c86bde000b3317dc7
handup123qwe@gmail.com <43237698+TienOptimum@users.noreply.github.com>
2020-06-07 22:03:32 +0700

    add new project

---
+++ hotelReservationManager/src/test/java/com/hotelReservationManager/hotelReservationManager/HotelReservationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelReservationManager.hotelReservationManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d9b7a9b79eab986b7b0f55188a18ef192f1dd916
handup123qwe@gmail.com <43237698+TienOptimum@users.noreply.github.com>
2020-06-10 12:21:27 +0700

    add new application

--- hotelReservationManager/src/test/java/com/hotelReservationManager/api/HotelReservationManagerApplicationTests.java
+++ hotelReservationManager/src/test/java/com/hotelReservationManager/api/HotelReservationManagerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hotelReservationManager.hotelReservationManager;
+package com.hotelReservationManager.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

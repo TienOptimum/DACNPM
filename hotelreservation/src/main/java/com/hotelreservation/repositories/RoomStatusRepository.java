8a73c6c4351bdb8c19a3b3fcb2e892926cf48a9e
handup123qwe@gmail.com <43237698+TienOptimum@users.noreply.github.com>
2020-07-26 15:02:03 +0700

    update repos + service + controller

--- hotelreservation/src/main/java/com/hotelreservation/repositories/RoomStatusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotelreservation.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomStatusRepository extends JpaRepository<RoomStatus, Integer>{
-
-}
